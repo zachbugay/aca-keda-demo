482874ade4d4f0dbbe0ea69d49819c5605686dab
Zach Bugay <zacharybugay@microsoft.com>
2026-04-09 11:58:14 -0400

    fix: add example frontend

---
+++ src/Bugay.Frontend.Example/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bugay.Frontend.Example.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
